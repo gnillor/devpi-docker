ced58c99d339727f31bb392fd62fe86374e8560b
gnillor <gump.zeng@gmail.com>
2023-09-18 13:18:28 +0800

    Update devpi server init process for new version

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,11 @@
 FROM python:3.9.18
 
-# Add the user of devpi
-RUN useradd -m -U -s /bin/bash devpi
-
 # Install packages
 RUN pip install --no-cache-dir -i https://mirrors.aliyun.com/pypi/simple/ \
-    "devpi-client==6.0.5" \
-    "devpi-web==4.2.1" \
-    "devpi-server==6.9.2"
+    "devpi-client" \
+    "devpi-web" \
+    "devpi-server" \
+    "devpi"
 
 EXPOSE 3141
 VOLUME /data
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+function generate_password() {
+    set +e
+    tr -cd '[:alnum:]' < /dev/urandom | fold -w30 | head -n1 | tr -cd '[:alnum:]'
+    set -e
+}
+
+DEVPI_ROOT_PASSWORD="${DEVPI_ROOT_PASSWORD:-}"
+if [ -f "$DEVPI_SERVERDIR/.root_password" ]; then
+    DEVPI_ROOT_PASSWORD=$(cat "$DEVPI_SERVERDIR/.root_password")
+elif [ -z "$DEVPI_ROOT_PASSWORD" ]; then
+    DEVPI_ROOT_PASSWORD=$(generate_password)
+fi
 
 # Set default storage paths
 function defaults {
@@ -14,19 +26,15 @@ function defaults {
 # Init devpi
 function init_devpi {
     echo "[RUN]: Init devpi-server"
-    devpi-server --init
-    devpi-server --start --host 127.0.0.1 --port 3141 --serverdir ${DEVPI_SERVERDIR}
-    devpi-server --status
-    devpi use http://localhost:3141
+    devpi-init --serverdir ${DEVPI_SERVERDIR}
+    nohup devpi-server --host 0.0.0.0 --port 3141 --serverdir ${DEVPI_SERVERDIR} > ${DEVPI_SERVERDIR}/devpi.log 2>&1 &
+    sleep 6s
 
-    devpi user -c admin password=admin
-    devpi login admin --password admin
+    devpi use http://localhost:3141
+    devpi login root --password=''
+    devpi user -m root "password=$DEVPI_ROOT_PASSWORD"
     devpi index -c douban type=mirror mirror_url=http://pypi.douban.com/simple
     devpi index -c aliyun type=mirror mirror_url=http://mirrors.aliyun.com/pypi/simple/
-    devpi index -c douban bases=admin/aliyun,admin/douban,root/pypi mirror_whitelist='*'
-    devpi index -y -c public mirror_whitelist='*'
-    devpi-server --stop
-    devpi-server --status
 }
 
 defaults
@@ -34,13 +42,23 @@ defaults
 if [ "$1" = 'devpi' ]; then
     if [ ! -f  $DEVPI_SERVERDIR/.serverversion ]; then
         init_devpi
+    else
+        echo "[RUN]: Launching devpi-server"
+        nohup devpi-server --host 0.0.0.0 --port 3141 --serverdir ${DEVPI_SERVERDIR} > ${DEVPI_SERVERDIR}/devpi.log 2>&1 &
     fi
-
-    echo "[RUN]: Launching devpi-server"
-    exec devpi-server --host 0.0.0.0 --port 3141
 fi
 
-echo "[RUN]: Builtin command not provided [devpi]"
-echo "[RUN]: $@"
+echo "Watching devpi-server"
+PID=$(pgrep devpi-server)
 
-exec "$@"
+if [ -z "$PID" ]; then
+    echo "ENTRYPOINT: Could not determine PID of devpi-server!"
+    exit 1
+fi
+
+while : ; do
+    kill -0 "$PID" > /dev/null 2>&1 || break
+    sleep 2s
+done
+
+echo "ENTRYPOINT: devpi-server died, exiting..."
